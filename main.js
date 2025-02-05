33993b4747cd6a0c72b750a3ce60ef17f006cf78
mikrovilu3 <martins.mikelsons7@gmail.com>
2025-02-05 23:55:14 +0200

    cut some finihed othr features

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 function nosūtīt(){
-    alert("Jūsu jautājums ir nosūtīts");
+    const lild =document.getElementById("lieldienas")
+    setTimeout(function(){alert("Jūsu jautājums ir nosūtīts");},100);
     startConfetti();
-    document.getElementById('manksausmes')
+    var vert = document.getElementById('vertejums');
+    if(vert.value=1){
+        lild.style.backgroundColor = red;
+        
+    }
 }
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('manksausmes').addEventListener('submit', function(event) {
       event.preventDefault();
       nosūtīt();
     });
-    console.log('added event listener')
+    console.log('added event listener');
   });
